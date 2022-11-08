4d2498f48722671baddacee40f99c62ddabb9ed7
USER_NAME <radams.cj@gmail.com>
2022-11-08 19:00:47 -0400

    my first commit

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+ /**
+   * This is a method.
+   */
+  public final String greet(final String someone) {
+    String password = "Admin@123";
+    return String.format("Hello Avinash, %s!", someone);
+  }
+}
